73c42e7a0c9f97b53eef99b381857daea67cdf74
zubeir <zubwd1@gmail.com>
2024-08-06 15:07:57 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>Mon compteur de clicks</h1>
+      <ClickCounter />
+    </div>
+</template>
+
+  <script setup>
+  import ClickCounter from './components/icons/ClickCounter.vue'
+  </script>
+
+  <style scoped>
+  </style>
+  
---
+++ src/components/icons/ClickCounter.vue
@@ -0,0 +1,31 @@
+ <template>
+    <div>
+      <button @click="increment">Cliquez-moi</button>
+      <p>Nombre de clics : {{ count }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+
+  const count = ref(10)
+  function increment() {
+    count.value++
+  }
+  </script>
+  
+  <style scoped>
+  button {
+    font-size: 1.2em;
+    padding: 10px;
+    margin: 10px;
+    background-color: aqua;
+    border-radius: 10px;
+    width: 50%;
+  }
+  
+  p {
+    font-size: 1.2em;
+  }
+  
+  </style>
